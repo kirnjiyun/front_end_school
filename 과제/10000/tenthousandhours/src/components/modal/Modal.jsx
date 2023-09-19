3d775071127714fe7fe897aaf7e7a3b097d8c7e0
kirnjiyun <kimjiyunee@naver.com>
2023-09-19 16:44:57 +0900

    modal 고쳤음

--- 과제/10000/tenthousandhours/src/components/modal/Modal.jsx
+++ 과제/10000/tenthousandhours/src/components/modal/Modal.jsx
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import LicatImg from '../../images/licat.png';
 import './Modal.css';
 
-export default function Modal() {
-    const [modalShow, setModalShow] = useState(true);
-
-    const closeModal = () => {
-        setModalShow(false);
-    };
-
+export default function Modal({ setModalShow }) {
     return (
-        <article id="modal" style={{ display: modalShow ? 'block' : 'none' }}>
+        <article id="modal">
             <div className="modal-wrap">
                 <h2>화이팅!!&#9829;&#9829;&#9829;</h2>
                 <h3>당신의 꿈을 응원합니다!</h3>
-                <img src={LicatImg} alt="라이캣의 응원" />
-                <button type="button" className="btn-close" onClick={closeModal}>
-                    종료하고 진짜 훈련하러 가기 GO!GO!
-                </button>
+                <img src={LicatImg} alt="라이켓의 응원" />
+                <button type="button" className="btn-close" onClick={() => setModalShow(false)}>종료하고 진짜 훈련하러 가기 GO!GO!</button>
             </div>
         </article>
-    );
+    )
 }
+
